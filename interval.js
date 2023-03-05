f4278f8ad7a8081aec4081a58cc8f909b999c96b
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-06 04:51:57 +0600

    setTimeOut and setInterval explore

---
+++ interval.js
@@ -0,0 +1,19 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// setTimeout(()=>{
+//     console.log(4);
+// },2000)
+// console.log(5);
+// console.log(6);
+
+console.log(1);
+console.log(2);
+console.log(3);
+let num=0;
+setInterval(()=>{
+    console.log(num++);
+},1000)
+console.log(5);
+console.log(6);
+
